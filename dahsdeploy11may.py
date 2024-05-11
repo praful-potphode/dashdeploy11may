97e9f89724c3605d071a8fec5bd6aba28a8df9ef
Praful <51020328+praful-potphode@users.noreply.github.com>
2024-05-11 12:28:35 +0530

    Update dahsdeploy11may.py

--- dahsdeploy11may.py
+++ dahsdeploy11may.py
@@ -8,7 +8,7 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 styles = {
     'pre': {
         'border': 'thin lightgrey solid',
